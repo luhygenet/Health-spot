01da5bd80ec089e3c2e37ba0cd7f4a09e96e5471
Liya Daniel <luhygenet@gmail.com>
2025-10-18 14:58:00 +0300

    changed logo

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Health-Spot</title>
+      <meta name="description" content="Health-Spot placeholder app" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Head from "./head";
+
 export default function Home() {
   return (
-    <main style={{ textAlign: "center", marginTop: "20%" }}>
-      <h1>Hello World — Health-spot</h1>
-      <p>Placeholder deployment (Next.js + TypeScript)</p>
-      <p>I hope you&apos;re working and connected to github</p>
-    </main>
+    <>
+      
+      {/* <head>
+      <title>Health-Spot</title>
+      <meta name="description" content="Health-Spot placeholder app" />
+      <link rel="icon" href="/favicon.ico" />
+      </head> */}
+      <main style={{ textAlign: "center", marginTop: "20%" }}>
+        <h1>Hello World — Health-spot</h1>
+        <p>Placeholder deployment (Next.js + TypeScript)</p>
+        <p>I hope you&apos;re working and connected to github</p>
+      </main>
+    </>
   );
 }
